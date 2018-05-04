6c401775822c44b4562517e1517aa287319f57a1
David Van Der Beek <earlynovrock@gmail.com>
2018-05-03 21:52:33 -0500

    fix column type, add foreign key (temp)

--- db/migrate/20180504001608_create_notepad_notes.rb
+++ db/migrate/20180504001608_create_notepad_notes.rb
@@ -1,11 +1,13 @@
 class CreateNotepadNotes < ActiveRecord::Migration[5.2]
   def change
     create_table :notepad_notes do |t|
-      t.string :body
+      t.text :body
       t.references :author, foreign_key: true
       t.references :notable, polymorphic: true
 
       t.timestamps
     end
+
+    add_foreign_key :notepad_notes, :admin_users, column: :author_id, on_delete: :cascade
   end
 end
